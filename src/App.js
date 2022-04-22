168bfc56ef6d7c7427e24e6472c2f5adf3f45dff
Kohlten Bills <kohltenbillsy@gmail.com>
2022-04-22 13:28:50 -0600

    complete all test past but codegrade says 0%

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const App = () => {
   useEffect(() => {
     axios.get("https://swapi.dev/api/people/")
       .then(res => {
-        // console.log(res.data)
         setData(res.data)
       })
       .catch(err => {
